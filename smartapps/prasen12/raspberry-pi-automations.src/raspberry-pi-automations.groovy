d9680bebfcbd6771b9a8f5e8c53378f4ca651d96
Prasen Palvankar <prasen.palvankar@oracle.com>
2017-09-09 15:05:24 -0700

    .

--- smartapps/prasen12/raspberry-pi-automations.src/raspberry-pi-automations.groovy
+++ smartapps/prasen12/raspberry-pi-automations.src/raspberry-pi-automations.groovy
@@ -26,39 +26,49 @@ definition(
 
 
 preferences {
-	page (name: "mainPage")
+    page (name: "mainPage")
     page (name: "schedulePage")
     page(name: "notAllowedPage")
     
 }
 
 def installed() {
-	log.debug "Installed with settings: ${settings}"               
-	initialize()
+    log.debug "Installed with settings: ${settings}"               
+    initialize()
 }
 
 def updated() {
-	log.debug "Updated with settings: ${settings}"
-	unschedule()
-	unsubscribe()
-	initialize()
+    log.debug "Updated with settings: ${settings}"
+    unschedule()
+    unsubscribe()
+    initialize()
 }
 
 def initialize() {
-	 def device = parent.getServiceDevice(station);
+    def device = parent.getServiceDevice(station);
     log.debug "Service Device = ${device}"   
-	app.updateLabel("Automation for ${device}");
+    app.updateLabel("Automation for ${device}");
     schedule(startTime, startSprinkler);
-    schedule(endTime, stopSprinkler);
+    
 }
 
 def startSprinkler() {
-	log.debug "Start sprinkler called"
-    parent.getServiceDevice(station).on()
+    log.debug "Start sprinkler called"
+    def df = new java.text.SimpleDateFormat("EEEE")
+    // Ensure the new date object is set to local time zone
+    df.setTimeZone(location.timeZone)
+    def day = df.format(new Date())
+    def dayCheck = days.contains(day);
+    if (dayCheck) {
+        def sprinkler = parent.getServiceDevice(station);
+        log.debug "Turning on sprinkler ${sprinkler.displayName}"
+        schedule(endTime, stopSprinkler);
+        sprinkler.on()
+    }
 }
 
 def stopSprinkler() {
-	log.debug "Stop sprinkler called"
+    log.debug "Stop sprinkler called"
     parent.getServiceDevice(station).off()
 }
 /**
@@ -69,7 +79,7 @@ def mainPage() {
     // Make sure this is not directly installed
     if (parent) {
     	schedulePage()
-      } 
+    } 
     else {
     	notAllowedPage()
     }
@@ -77,30 +87,49 @@ def mainPage() {
 }
 
 def notAllowedPage () {
-	dynamicPage(name: "notAllowedPage", title: "This install Method is Not Allowed", install: false, uninstall: true) {
-		section() {
-			paragraph "Raspberry Pi Automations can't be directly installed from the Marketplace.\n\nPlease use the Raspberry Pi Connect SmartApp to configure them.", required: true,
-			state: null, image: getAppImg("disable_icon2.png")
-		}
-	}
+    dynamicPage(name: "notAllowedPage", title: "This install Method is Not Allowed", install: false, uninstall: true) {
+        section() {
+            paragraph "Raspberry Pi Automations can't be directly installed from the Marketplace.\n\nPlease use the Raspberry Pi Connect SmartApp to configure them.", required: true,
+            state: null, image: getAppImg("disable_icon2.png")
+        }
+    }
 }
 
 def schedulePage() {
-	dynamicPage(name: "schedulePage", title: "Sprinkler Schedule", install: true, uninstall: true) {    	
-                section("Which Station to Automate?") {
-                	input (name: "station", title: "Station", type: "device.raspberryPiIrrigationStation");
-            	}    
-            	section("At what time?") {
-                	input (name: "startTime", title: "Start Time", type:"time");
-            	}
-                section("At what time do you want it turned off?") {
-                	input (name: "endTime", title: "Turn off at", type: "time");
-                }
-                section("Which days?") {
-                	input "days", "enum", multiple: true, title: "Run on specific day(s)", description: "Choose Days", required: true, options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]				
-                }
+    dynamicPage(name: "schedulePage", title: "Sprinkler Schedule", install: true, uninstall: true) {   
+        section("Which Station to Automate?") {
+            input (name: "station", title: "Station", type: "device.raspberryPiIrrigationStation");
+            input (name: "automationName", title: "Automation Name", type: "string")
+        }    
+        section("Schedule") {
+            input (name: "startTime", title: "Start Time", type:"time")
+            input (name: "endTime", title: "Turn off at", type: "time");
+            input (name: "scheduleDays", type: "enum", multiple: true, title: "Run on specific day(s)", description: "Choose Days", required: true, options: getWeekDays());			
+        }
+        section("Don't turn on the sprinkler if ... ") {
+            input (name: "skipOnRainDay", title: "Rain in forecast for the scheduled day", type: "boolean", required: false)
+            input (name: "skipIfRained", title: "It has rained  between 1 and 7 days ago", type: "number", range: "1..7", required: false)                        
+        }
+        section("Increase the sprinkler time if ...") {
+            input (name: "incrementDuration", title: "Temperature forecast is for scheduled day is greater than ", type: "number", required: false)
+        }
                
 		   	
-        }
+    }
 }
-// TODO: implement event handlers
+
+def buildCronrabSchedule(){
+    def d = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ").parse(startTime);
+    d.setTimeZone(location.timeZone);
+    
+    def a = ​new java.text.SimpleDateFormat("'0 'mm​' 'HH' * '")​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​.format(d)​
+    def days = [];
+    scheduleDays.each {
+        
+    }
+    def days = getWeekDays().indexOf()
+}
+
+def getWeekDays() {
+    ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+}
